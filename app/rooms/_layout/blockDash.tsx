0cf5f62d6aaaffd078f1e4c66b3084f9c361bdb8
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2025-09-06 07:14:48 +0100

    Refactor BlockDash layout to improve structure and add NewUser component
    This commit enhances the BlockDash component by restructuring the layout for better readability and organization. It introduces the NewUser component alongside the existing NewRoom component, allowing for the addition of new users within the dashboard. The overall layout is adjusted to maintain a clean and user-friendly interface.

--- app/rooms/_layout/blockDash.tsx
+++ app/rooms/_layout/blockDash.tsx
@@ -5,26 +5,33 @@ import CardWidgets from "./cardWidgets";
 import TableRooms from "./tableRooms";
 import { useModalContext } from "@/contexts/Modal/ModalContext";
 import NewRoom from "@/components/features/room/newRoom";
+import NewUser from "@/components/features/room/newUser";
 
 const BlockDash = () => {
   const { openModal } = useModalContext();
   return (
-    <div className='container-fluid lg:px-7 px-2'>
-      <div className="flex items-center justify-between lg:mb-4">
-        <h2 className="text-colorTitle font-semibold lg:mb-3 mb-4 text-[18px]">
-          Comptes rendus des réunions
-        </h2>
-        <Button className="bg-primaryColor text-white" onPress={() => openModal("ModalNewRoom")}>
-          <Plus size="18"></Plus>
-          Nouvelle réunion
-        </Button>
+    <>
+      <div className="container-fluid lg:px-7 px-2">
+        <div className="flex items-center justify-between lg:mb-4">
+          <h2 className="text-colorTitle font-semibold lg:mb-3 mb-4 text-[18px]">
+            Comptes rendus des réunions
+          </h2>
+          <Button
+            className="bg-primaryColor text-white"
+            onPress={() => openModal("ModalNewRoom")}
+          >
+            <Plus size="18"></Plus>
+            Nouvelle réunion
+          </Button>
+        </div>
+        <div className="grid grid-col-1 gap-3 lg:gap-4">
+          <CardWidgets />
+          <TableRooms />
+        </div>
       </div>
-      <div className="grid grid-col-1 gap-3 lg:gap-4">
-        <CardWidgets />
-        <TableRooms />
-      </div>
-      <NewRoom/>
-    </div>
+      <NewRoom />
+      <NewUser />
+    </>
   );
 };
 
